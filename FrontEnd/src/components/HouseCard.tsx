ab2ada7c9916e0c1501809f3554f77689bd1ed4d
Amit Bar <amitbar00@gmail.com>
2021-03-10 22:57:50 -0800

    got house card to work but it's kinda ugly cause of the photos

--- FrontEnd/src/components/HouseCard.tsx
+++ FrontEnd/src/components/HouseCard.tsx
@@ -15,6 +15,7 @@ interface Props {
 
 const HouseCard: React.FC<Props> = ({ roomId }) => {
   const { data, error } = useRoomData(roomId);
+  const router = useRouter();
 
   if (error) {
     return <div>Error occurred!</div>; // TODO handle error in a different way
@@ -37,86 +38,74 @@ const HouseCard: React.FC<Props> = ({ roomId }) => {
     distance,
   } = data;
 
-  const photos = useRemotePhotos(unconvertedPhotos);
-  const [slideShowItems, setSlideShowItems] = useState<SlideShowItem[]>([]);
-  const router = useRouter();
-
   // set the slide show items
-  useEffect(() => {
-    setSlideShowItems(
-      photos.map((url) => ({
-        src: url,
-        alt: `${leaserEmail} , ${location}}`,
-      })),
-    );
-  }, [setSlideShowItems, photos, leaserEmail, location]);
+  const slideShowItems = useRemotePhotos(unconvertedPhotos).map((url) => ({
+    src: url,
+    alt: `${leaserEmail} , ${location}}`,
+  }));
 
   return (
-    <>
-      <Card className="house-card">
-        <Card.Body className="p-0">
-          <Container>
-            <Row className="house-pic">
-              <SlideShow
-                images={slideShowItems}
-                onImageClick={() => router.push(`/housing/${roomId}`)}
-              />
-            </Row>
+    <Card className="house-card">
+      <Card.Body className="p-0">
+        <Container>
+          <Row className="house-pic">
+            <SlideShow
+              images={slideShowItems}
+              onImageClick={() => router.push(`/housing/${roomId}`)}
+            />
+          </Row>
 
-            {/* 1st row */}
-            <Row className="px-2">
-              <Col md={6} className="price-related-large-text">
-                <Row>
-                  {negotiable && '~'}${pricePerMonth}
-                </Row>
-              </Col>
-              <Col md={6} className="pt-2">
-                <Row>
-                  <div className="w-100 text-right secondary-text">
-                    {formatRoomType(roomType)}
-                    <span className="divider"> | </span>{' '}
-                    {`${numBeds} B ${numBaths} Ba`}
-                  </div>
-                </Row>
-              </Col>
-            </Row>
+          {/* 1st row */}
+          <Row className="px-2">
+            <Col md={6} className="price-related-large-text">
+              <Row>
+                {negotiable && '~'}${pricePerMonth}
+              </Row>
+            </Col>
+            <Col md={6} className="pt-2">
+              <Row>
+                <div className="w-100 text-right secondary-text">
+                  {formatRoomType(roomType)}
+                  <span className="divider"> | </span>{' '}
+                  {`${numBeds} B ${numBaths} Ba`}
+                </div>
+              </Row>
+            </Col>
+          </Row>
 
-            {/* 2nd row */}
-            <Row className="px-2">
-              <Col md={6}>
-                <Row className="address-related-text">
-                  <b>~ {distance}</b>&nbsp;transit
-                </Row>
-              </Col>
-              <Col md={6}>
-                <Row>
-                  <div className="w-100 text-right secondary-text text-truncate">
-                    Move in {formattedMoveIn}
-                  </div>
-                </Row>
-              </Col>
-            </Row>
+          {/* 2nd row */}
+          <Row className="px-2">
+            <Col md={6}>
+              <Row className="address-related-text">
+                <b>~ {distance}</b>&nbsp;transit
+              </Row>
+            </Col>
+            <Col md={6}>
+              <Row>
+                <div className="w-100 text-right secondary-text text-truncate">
+                  Move in {formattedMoveIn}
+                </div>
+              </Row>
+            </Col>
+          </Row>
 
-            {/* 3rd row */}
-            <Row className="px-2">
-              <Col md={6} className="address-related-text">
-                {/* {distance} To Price Center */}
-                {/* <Row>{distance}</Row> */}
-                <Row>To Price Center</Row>
-              </Col>
+          {/* 3rd row */}
+          <Row className="px-2">
+            <Col md={6} className="address-related-text">
+              {/* {distance} To Price Center */}
+              {/* <Row>{distance}</Row> */}
+              <Row>To Price Center</Row>
+            </Col>
 
-              <Col md={6} className="secondary-text">
-                <Row>
-                  <div className="w-100 text-right text-truncate">
-                    {location}
-                  </div>
-                </Row>
-              </Col>
-            </Row>
-          </Container>
-        </Card.Body>
-      </Card>
-    </>
+            <Col md={6} className="secondary-text">
+              <Row>
+                <div className="w-100 text-right text-truncate">{location}</div>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </Card.Body>
+    </Card>
   );
 };
 
