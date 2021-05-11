14cf90c229aeead74d428985666bd147ceaa7581
Amit Bar <amitbar00@gmail.com>
2021-05-10 17:18:56 -0700

    can handle SVG files in storybook now

--- FrontEnd/.storybook/main.js
+++ FrontEnd/.storybook/main.js
@@ -24,5 +24,39 @@ module.exports = {
         },
       },
     },
-  ]
+  ],
+  webpackFinal: async (config) => {
+    // Needed for SVG importing using svgr
+    // find index of the rule to handle SVG's
+    const indexOfRuleToRemove = config.module.rules.findIndex(
+      (rule) => rule.test && rule.test.toString().includes('svg')
+    );
+
+    // replace that rule with a rule to load other files (use 'file-loader' package to do so)
+    config.module.rules.splice(indexOfRuleToRemove, 1, {
+      test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/,
+      loader: require.resolve('file-loader'),
+      options: {
+        name: 'static/media/[name].[hash:8].[ext]',
+        esModule: false,
+      },
+    })
+
+    // add a new rule to handle SVG files with '@svgr/webpack' package
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+          },
+        },
+      ],
+    })
+
+    // Merge your next webpack config with this config
+    const nextConfig = require('../next.config.js');
+    return { ...nextConfig.webpack, ...config };
+  },
 }
