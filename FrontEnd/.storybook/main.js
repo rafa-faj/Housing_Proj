09a2b86aafd682f5f74559ba8cd77de7bff3a574
Amit Bar <amitbar00@gmail.com>
2021-05-10 17:08:05 -0700

    changed comments in configuration

--- FrontEnd/.storybook/main.js
+++ FrontEnd/.storybook/main.js
@@ -13,10 +13,13 @@ module.exports = {
           modules: true,
         },
         sassLoaderOptions: {
-          implementation: require("sass"),
           sassOptions: {
+            // include the path to the scss folder for easy access (it allows you to do things like
+            // "@import 'utils'" without having to specify the path)
             includePaths: [path.join(__dirname, '../src/assets/scss')],
           },
+          // Prepend the following line to every scss file (no need to import to use sass
+          // variables and other utils in every file)
           additionalData: `@use 'utils' as *;`,
         },
       },
--- FrontEnd/next.config.js
+++ FrontEnd/next.config.js
@@ -19,7 +19,8 @@ module.exports = withPlugins([
     // include the path to the scss folder for easy access (it allows you to do things like
     // "@import 'utils'" without having to specify the path)
     includePaths: [path.join(__dirname, 'src/assets/scss')],
-    // Prepend the following line to every scss file (no need to import to use sass variables)
+    // Prepend the following line to every scss file (no need to import to use sass
+    // variables and other utils in every file)
     prependData: `@use 'utils' as *;`
   },
   webpack: (config, options) => {
