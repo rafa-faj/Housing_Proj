03e75ae6558adee01a20bef07ad58c60ff3333bd
Bao Hoang <bghoang@ucsd.edu>
2021-05-19 13:38:05 -0700

    amit approve

--- FrontEnd/src/components/HouseCardList/HouseCard.tsx
+++ FrontEnd/src/components/HouseCardList/HouseCard.tsx
@@ -48,6 +48,42 @@ const HouseCard: FunctionComponent<Props> = ({ roomId }) => {
     alt: `${leaserEmail} , ${address}}`,
   }));
 
+  const textCol = (
+    <Col md={5} className={styles.secondCol}>
+      <div className={styles.textPortion}>
+        <div className={styles.day}>
+          <miscIcons.RoundArrow />  2 days ago
+                </div>
+        <div className={styles.price}>
+          Starting at <b>$1200/mo </b>
+        </div>
+        <div className={styles.distance}>
+          <miscIcons.busIcon /> <b>~ {distance} transit</b>&nbsp;
+        </div>
+        <div className={styles.address}>
+          <div className={styles.locationIcon}>
+            <miscIcons.LocationIcon />
+          </div>
+          <div>
+            {address}
+          </div>
+        </div>
+        <div>
+          <div className={styles.room}>
+            {`${numBeds}Bed ${numBaths}Bath`}
+            <span className={styles.divider}> | </span>{' '}
+            {formatRoomType(roomType)} Room
+          </div>
+        </div>
+        <div>
+          <div className={styles.date}>
+            Available from June 1, 2021{/*{formattedMoveIn}*/}
+          </div>
+        </div>
+      </div>
+    </Col>
+  )
+
   return (
     <Card className={styles.card}>
       <Card.Body className="p-0">
@@ -59,103 +95,8 @@ const HouseCard: FunctionComponent<Props> = ({ roomId }) => {
                 onImageClick={() => routeToHouseProfile(roomId)}
               />
             </Col>
-            <Col md={5} className={styles.secondCol}>
-              <div className={styles.textPortion}>
-                {/* 1st row */}
-                <div className={styles.day}>
-                  <miscIcons.RoundArrow />  2 days ago
-                </div>
-                {/* 2nd row */}
-                <div className={styles.price}>
-                  Starting at <b>$1200/mo </b>
-                </div>
-                {/* 3rd row */}
-                <div className={styles.distance}>
-                  <miscIcons.busIcon /> <b>~ {distance} transit</b>&nbsp;
-               </div>
-                {/* 4th row */}
-                <div className={styles.address}>
-                  <div className={styles.locationIcon}>
-                    <miscIcons.LocationIcon />
-                  </div>
-                  <div>
-                    {address}
-                  </div>
-                </div>
-                {/* 5th row */}
-                <div>
-                  <div className={styles.room}>
-                    {`${numBeds}Bed ${numBaths}Bath`}
-                    <span className={styles.divider}> | </span>{' '}
-                    {formatRoomType(roomType)} Room
-                </div>
-                </div>
-                {/* 6th row */}
-                <div>
-                  <div className={styles.date}>
-                    Available from June 1, 2021{/*{formattedMoveIn}*/}
-                  </div>
-                </div>
-              </div>
-            </Col>
+            {textCol}
           </Row>
-          {/* Old design */}
-          {/*           
-          <Row className={styles.pic}>
-            <SlideShow
-              images={slideShowItems}
-              onImageClick={() => routeToHouseProfile(roomId)}
-            />
-          </Row> */}
-
-          {/* 1st row */}
-          {/* <Row className="px-2">
-            <Col md={6} className="price-related-large-text">
-              <Row>
-                {negotiable && '~'}${pricePerMonth}
-              </Row>
-            </Col>
-            <Col md={6} className="pt-2">
-              <Row>
-                <div className="w-100 text-right secondary-text">
-                  {formatRoomType(roomType)}
-                  <span className={styles.divider}> | </span>{' '}
-                  {`${numBeds} B ${numBaths} Ba`}
-                </div>
-              </Row>
-            </Col>
-          </Row> */}
-
-          {/* 2nd row */}
-          {/* <Row className="px-2">
-            <Col md={6}>
-              <Row className="address-related-text">
-                <b>~ {distance}</b>&nbsp;transit
-              </Row>
-            </Col>
-            <Col md={6}>
-              <Row>
-                <div className="w-100 text-right secondary-text text-truncate">
-                  Move in {formattedMoveIn}
-                </div>
-              </Row>
-            </Col>
-          </Row> */}
-
-          {/* 3rd row */}
-          {/* <Row className="px-2">
-            <Col md={6} className="address-related-text"> */}
-          {/* {distance} To Price Center */}
-          {/* <Row>{distance}</Row> */}
-          {/* <Row>To Price Center</Row>
-            </Col>
-
-            <Col md={6} className="secondary-text">
-              <Row>
-                <div className="w-100 text-right text-truncate">{address}</div>
-              </Row>
-            </Col>
-          </Row> */}
         </Container>
       </Card.Body>
     </Card>
