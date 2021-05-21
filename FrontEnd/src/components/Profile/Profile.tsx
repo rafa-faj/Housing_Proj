46d186b1f5fe0ab8086381b061c242fb00e33362
Will Hsu <willhsu@Wills-MacBook-Air.local>
2021-05-21 16:46:20 -0700

    Profile Page WIP

--- FrontEnd/src/components/Profile/Profile.tsx
+++ FrontEnd/src/components/Profile/Profile.tsx
@@ -11,11 +11,6 @@ import { User, dummyUser } from '@models';
 import styles from './Profile.module.scss';
 import cn from 'classnames';
 
-interface PathProps {
-  show: boolean;
-  setShow: (show: boolean) => void;
-}
-
 const phoneFormat = (phone: string, previousPhone: string) => {
   const phoneRegex = /\d+/;
   const charArray = [];
@@ -80,32 +75,230 @@ const Profile: FunctionComponent = () => {
   const [userSelectedDraft, setUserSelectedDraft] = useState(dummyUser); // TODO old code was:  useSelector(selectUserDraft) || dummyUser;
   const dispatch = useDispatch();
   const [activeIndicator, setactiveIndicator] = useState(true);
-  const [editPosts, setEditPosts] = useState(false);
+  const [viewMyPosts, setViewMyPosts] = useState(false);
 
   return (
-      <div className="h-100 w-100">
-        <Col className={`px-3 py-2 ${styles.topBar}`}>
+    <Container>
+      <Row>
+        <Col md={3} className={styles.selectionList}>
           <div
-            className={cn(styles.title, {
-              [styles.profileSelected]: editPosts,
-            })}
-            onClick={() => setEditPosts(false)}
-          >
-            Profile
-            {!editPosts && <div className={styles.element}>_____</div>}
+            className={cn(styles.titleNotSelected, {[styles.selectOn]: viewMyPosts})}
+            onClick={() => setViewMyPosts(true)}>
+            <span className={cn(styles.divider, {[styles.dividerOff]: !viewMyPosts})}></span>
+            <div className={styles.selectItem}>My Posts</div>
           </div>
-          <div className={`${styles.divider} mx-5`}>|</div>
-          <span
-            className={cn(styles.title, {
-              [styles.profileSelected]: editPosts,
-            })}
-            onClick={() => setEditPosts(true)}
-          >
-            My posts
-            {editPosts && <div className={styles.element}>_____________</div>}
-          </span>
+          <div className={cn(styles.titleSelected, {[styles.selectOff]: viewMyPosts})}
+            onClick={() => setViewMyPosts(false)}> 
+            <span className={cn(styles.divider, {[styles.dividerOff]: viewMyPosts})}></span>
+            <div className={styles.selectItem}>Profile</div>
+          </div>
         </Col>
+        <Col className={styles.selectionList}>
         <div className={`my-4 px-4 ${styles.middleSection}`}>
+          <Row>
+            <div className={styles.profileWrap}>
+              <Image
+                src={`https://houseit.s3.us-east-2.amazonaws.com/${userSelectedDraft.profilePhoto}`} // TODO shouldnt use constant for the src
+                roundedCircle
+                className={styles.icon}
+              />
+            </div>
+            <div className={styles.name}>{userSelectedDraft.name}</div>
+            <div className={styles.verified}>
+              <profileIcons.tickMark />
+              <span className={styles.smallText}>UCSD Email Verified</span>
+            </div>
+            <div
+              className={`mt-5 ${styles.editWrap} ${styles.profileWrap}`}
+            >
+              {activeIndicator ? (
+                <Button
+                  variant="secondary"
+                  onClick={() => setactiveIndicator(false)}
+                >
+                  Edit
+                </Button>
+              ) : (
+                <div>
+                  <div>
+                    <Button
+                      variant="secondary"
+                      onClick={() => {
+                        const updates = generateUpdates(
+                          userSelected,
+                          userSelectedDraft,
+                        );
+                        // if nothing changes upon confirm, no need to send to backend
+                        if (
+                          !(
+                            Object.keys(updates).length === 0 &&
+                            updates.constructor === Object
+                          )
+                        ) {
+                          // TODO
+                          // dispatch(
+                          //   editProfile(
+                          //     userSelected.email,
+                          //     userSelectedDraft,
+                          //     generateUpdates(
+                          //       userSelected,
+                          //       userSelectedDraft,
+                          //     ),
+                          //     setactiveIndicator,
+                          //   ),
+                          // );
+                        } else {
+                          setactiveIndicator(true);
+                        }
+                        // TODO: display error if fails at backend
+                      }}
+                    >
+                      Confirm
+                    </Button>
+                  </div>
+                  <div className={`mt-1 ${styles.profileWrap}`}>
+                    <Button
+                      className={styles.cancel}
+                      variant="no-show"
+                      onClick={() => {
+                        setactiveIndicator(true);
+                        setUserSelectedDraft(userSelected);
+                      }}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Row>
+            <Row className="h-100">
+              <Col md={8} className={cn({ [styles.postsList]: viewMyPosts })}>
+                {!viewMyPosts ? (
+                  <>
+                    <Form.Row className="justify-content-center m-2">
+                      <Form.Group as={Col} controlId="profileEmail">
+                        <Form.Control
+                          type="email"
+                          disabled
+                          value={userSelectedDraft.email}
+                        />
+                      </Form.Group>
+                      <Form.Group as={Col} controlId="profilePhone">
+                        <Form.Control
+                          readOnly={activeIndicator}
+                          type="text"
+                          value={userSelectedDraft.phone}
+                          onChange={(event) => {
+                            console.log(userSelected, 'hello');
+                            const previousPhone = userSelectedDraft.phone;
+                            setUserSelectedDraft({
+                              ...userSelectedDraft,
+                              phone: phoneFormat(
+                                event.target.value,
+                                previousPhone,
+                              ),
+                            });
+                          }}
+                        />
+                      </Form.Group>
+                    </Form.Row>
+                    {/* tenary form for toggle group and display as a string */}
+                    <Form.Row className="m-2 px-0">
+                      <Form.Group as={Col} controlId="profileSchoolYear">
+                        <Form.Label className={styles.label}>
+                          School Year
+                        </Form.Label>
+                        <Form.Row className={`pl-1 ${styles.schoolYear}`}>
+                          {!activeIndicator ? (
+                            <ToggleGroup
+                              singleSelect
+                              content={Object.values(SchoolYear)}
+                              initialSelected={userSelectedDraft.schoolYear}
+                              onSelect={({ label }) => {
+                                setUserSelectedDraft({
+                                  ...userSelectedDraft,
+                                  schoolYear: label as SchoolYear,
+                                });
+                              }}
+                            />
+                          ) : (
+                            <Input
+                              type="text"
+                              value={userSelectedDraft.schoolYear}
+                              readOnly
+                              placeholder="School Year"
+                            />
+                          )}
+                        </Form.Row>
+                      </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row className="m-2">
+                      <Form.Group as={Col} controlId="profileMajor">
+                        <Form.Label className={styles.label}>Major</Form.Label>
+                        {!activeIndicator ? (
+                          <Dropdown
+                            options={majors}
+                            label=""
+                            onSelect={(s) => {
+                              setUserSelectedDraft({
+                                ...userSelectedDraft,
+                                major: s || userSelectedDraft.major,
+                              });
+                            }}
+                            initialSelected={userSelectedDraft.major}
+                            placeholder="Major"
+                          />
+                        ) : (
+                          <Input
+                            type="text"
+                            value={userSelectedDraft.major}
+                            readOnly
+                            placeholder="Major"
+                          />
+                        )}
+                      </Form.Group>
+                    </Form.Row>
+                    <Form.Row className="m-2">
+                      <Form.Group as={Col} controlId="profileBio">
+                        <Form.Label className={styles.label}>
+                          Short bio
+                        </Form.Label>
+                        <Form.Control
+                          readOnly={activeIndicator}
+                          as="textarea"
+                          className={styles.bioText}
+                          type="text"
+                          maxLength={600}
+                          value={userSelectedDraft.description}
+                          onChange={(event) =>
+                            setUserSelectedDraft({
+                              ...userSelectedDraft,
+                              description: event.target.value,
+                            })
+                          }
+                        />
+                        <span className={styles.charCheck}>
+                          {userSelectedDraft.description.length}/600
+                        </span>
+                      </Form.Group>
+                    </Form.Row>
+                  </>
+                ) : (<></>)}
+              </Col>
+            </Row>
+        </div>
+      
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+/**
+ * <Col>
+      <div className={`my-4 px-4 ${styles.middleSection}`}>
           <Container fluid className="h-100">
             <Row className="h-100">
               <Col md={4} className="align-self-center">
@@ -197,8 +390,8 @@ const Profile: FunctionComponent = () => {
                   )}
                 </div>
               </Col>
-              <Col md={8} className={cn({ [styles.postsList]: editPosts })}>
-                {!editPosts ? (
+              <Col md={8} className={cn({ [styles.postsList]: viewMyPosts })}>
+                {!viewMyPosts ? (
                   <>
                     <Form.Row className="justify-content-center m-2">
                       <Form.Group as={Col} controlId="profileEmail">
@@ -227,7 +420,7 @@ const Profile: FunctionComponent = () => {
                         />
                       </Form.Group>
                     </Form.Row>
-                    {/* tenary form for toggle group and display as a string */}
+                    {
                     <Form.Row className="m-2 px-0">
                       <Form.Group as={Col} controlId="profileSchoolYear">
                         <Form.Label className={styles.label}>
@@ -287,7 +480,7 @@ const Profile: FunctionComponent = () => {
                     <Form.Row className="m-2">
                       <Form.Group as={Col} controlId="profileBio">
                         <Form.Label className={styles.bio}>
-                          Tell us about yourself in a short bio
+                          Short bio
                         </Form.Label>
                         <Form.Control
                           readOnly={activeIndicator}
@@ -352,8 +545,8 @@ const Profile: FunctionComponent = () => {
             </Row>
           </Container>
         </div>
-      </div>
-  );
-};
+      </Col>
+
+ */
 
 export default Profile;
