0935d0faadf976e542d86df06ab1ef3ac9863fde
Amit Bar <amitbar00@gmail.com>
2021-05-21 15:28:30 -0700

    setup template + routing for house profile

---
+++ FrontEnd/src/components/HouseProfile/ApplicationDetails/ApplicationDetails.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+import styles from './ApplicationDetails.module.scss';
+
+interface ApplicationDetailsProps {}
+
+const ApplicationDetails: FunctionComponent<ApplicationDetailsProps> = ({}) => {
+  return <div></div>;
+};
+
+export default ApplicationDetails;
--- FrontEnd/src/components/HouseProfile/HouseProfile.tsx
+++ FrontEnd/src/components/HouseProfile/HouseProfile.tsx
@@ -1,59 +1,15 @@
 import React, { FunctionComponent } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import { HousePostUIData } from '../../models/PostModels';
-import FirstColumn from './FirstColumn/FirstColumn';
-import SecondColumn from './SecondColumn/SecondColumn';
-import ThirdColumn from './ThirdColumn/ThirdColumn';
 import { useRoomData } from '@hooks';
-import { Button } from 'react-bootstrap';
-import styles from './HouseProfile.module.scss';
-import cn from 'classnames';
+import GeneralInfo from './GeneralInfo/GenerlInfo';
+import PlaceDetails from './PlaceDetails/PlaceDetails';
+import ApplicationDetails from './ApplicationDetails/ApplicationDetails';
 
-interface PreviewButtonsProps {
-  onExit: () => any;
-  onPublish: () => any;
+interface HouseProfileProps {
+  roomId: number;
 }
 
-const PreviewButtons: FunctionComponent<PreviewButtonsProps> = ({
-  onExit,
-  onPublish,
-}) => (
-  <div className={styles.previewButtonsWrapper}>
-    <div className={styles.previewButtons}>
-      <Button variant="secondary" onClick={onExit}>
-        Edit Post
-      </Button>
-      <Button variant="secondary" onClick={onPublish}>
-        Publish Post
-      </Button>
-    </div>
-  </div>
-);
-
-type CommonProps = {
-  show: boolean;
-  onExit: () => any;
-};
-
-export interface PreviewData extends Omit<HousePostUIData, 'photos'> {
-  photos: File[];
-}
-
-type HandlePreview =
-  | { preview?: false; roomId: number }
-  | { preview: true; data: PreviewData; onPublish: () => any };
-
-type HouseProfileProps = CommonProps & HandlePreview;
-
-const HouseProfile: FunctionComponent<HouseProfileProps> = (props) => {
-  // object destructuring makes discriminated unions not possible, so keep everything in props and destructure within the function (look at homehub docs for more info)
-  const { show, onExit } = props;
-
-  const { data, error } = props.preview
-    ? { data: props.data, error: undefined }
-    : useRoomData(props.roomId);
+const HouseProfile: FunctionComponent<HouseProfileProps> = ({ roomId }) => {
+  const { data, error } = useRoomData(roomId);
 
   if (error) {
     return <div>Error occurred!</div>; // TODO handle error in a different way
@@ -85,53 +41,13 @@ const HouseProfile: FunctionComponent<HouseProfileProps> = (props) => {
   } = data;
 
   return (
-    <Modal
-      show={show}
-      onHide={onExit}
-      size="xl"
-      centered
-      className={cn({ [styles.preview]: props.preview })}
-      contentClassName={styles.modalContent}
-    >
-      {props.preview && (
-        <PreviewButtons onExit={onExit} onPublish={props.onPublish} />
-      )}
+    <div>
+      <GeneralInfo />
 
-      <Container className={styles.container}>
-        <Row>
-          <FirstColumn
-            leaserEmail={leaserEmail}
-            address={address}
-            photos={photos}
-            onExit={onExit}
-          />
+      <PlaceDetails />
 
-          <SecondColumn
-            name={name}
-            negotiable={negotiable}
-            pricePerMonth={pricePerMonth}
-            roomType={roomType}
-            stayPeriod={stayPeriod}
-            facilities={facilities}
-            formattedMoveIn={formattedMoveIn}
-            other={other}
-          />
-
-          <ThirdColumn
-            distance={distance}
-            roomDescription={roomDescription}
-            profilePhoto={profilePhoto}
-            leaserEmail={leaserEmail}
-            leaserName={leaserName}
-            leaserSchoolYear={leaserSchoolYear}
-            leaserMajor={leaserMajor}
-            leaserPhone={leaserPhone}
-            address={address}
-            roomId={props.preview ? undefined : props.roomId}
-          />
-        </Row>
-      </Container>
-    </Modal>
+      <ApplicationDetails />
+    </div>
   );
 };
 
