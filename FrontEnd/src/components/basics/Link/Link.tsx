2d54edef824896a1284e1fdfefa87d5c82035ab9
Amit Bar <amitbar00@gmail.com>
2021-05-25 01:49:30 -0700

    updated link to work with next link

--- FrontEnd/src/components/basics/Link/Link.tsx
+++ FrontEnd/src/components/basics/Link/Link.tsx
@@ -1,20 +1,38 @@
-// TODO need to merge this to work with next/link
-
-import React, {
-  FunctionComponent,
-  DetailedHTMLProps,
-  HTMLAttributes,
-} from 'react';
+import React, { FunctionComponent } from 'react';
 import styles from './Link.module.scss';
 import cn from 'classnames';
+import NextJSLink, { LinkProps as NextJSLinkProps } from 'next/link';
+import { isRunningInStorybook } from '@utils';
 
-export type LinkProps = DetailedHTMLProps<
-  HTMLAttributes<HTMLDivElement>,
-  HTMLDivElement
->;
+interface LinkProps extends NextJSLinkProps {
+  className?: string;
+}
 
-const Link: FunctionComponent<LinkProps> = ({ className, ...props }) => (
-  <div className={cn(className, styles.link)} {...props} />
-);
+/**
+ * Custom Link component, which wraps NextJS's link. Can be used in Storybook.
+ * Provides the correct styling for a link.
+ */
+const Link: FunctionComponent<LinkProps> = ({
+  className,
+  children,
+  href,
+  ...props
+}) => {
+  const joinedClassNames = cn(styles.link, className);
+
+  if (isRunningInStorybook()) {
+    return (
+      <a href={href.toString()} {...props}>
+        <div className={joinedClassNames}>{children}</div>
+      </a>
+    );
+  }
+
+  return (
+    <NextJSLink href={href} {...props}>
+      <div className={joinedClassNames}>{children}</div>
+    </NextJSLink>
+  );
+};
 
 export default Link;
