3d11b999290a5751540f7a96ab5fe1d64a26bbe5
yizong98 <yijianzong@gmail.com>
2021-05-29 21:05:00 -0700

    make the contact info appear on top

--- FrontEnd/src/components/HouseProfile/PlaceDetails/PlaceDetails.tsx
+++ FrontEnd/src/components/HouseProfile/PlaceDetails/PlaceDetails.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import Contact from './Contact/Contact';
 import styles from './PlaceDetails.module.scss';
+import styles_parent from '../HouseProfile.module.scss'
 import { useLandlordRoomData } from '@hooks';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -12,6 +13,25 @@ import cn from 'classnames';
 
 interface PlaceDetailsProps {
   roomId: number;
+}
+
+interface ColSpanConfig{
+  xs: number;
+  sm: number;
+  md: number;
+  lg: number;
+}
+
+interface RentAndHouseDetailsKeys{
+  Availability: HTMLElement;
+  'Lease Term': string;
+  'Pet Policy': HTMLElement;
+  Parking: string;
+  'Utility Details':string;
+}
+
+interface RentAndHouseDetailsProps extends ColSpanConfig{
+  details: RentAndHouseDetailsKeys;
 }
 
 const PlaceDetails: FunctionComponent<PlaceDetailsProps> = ({ roomId }) => {
@@ -39,80 +59,90 @@ const PlaceDetails: FunctionComponent<PlaceDetailsProps> = ({ roomId }) => {
   } = data;
 
   // Used in RentAndHouseDetails to display information with labels
-  const details = {
+  const detailsInCol = {
     Availability: (
       <span>
         The apartment is available <b>{availability}</b>
       </span>
     ),
-    'Lease Term': leaseTerm,
+    'Lease Term': leaseTerm
+  };
+  const detailsInColColConfigurations = {
+    xs: 12,
+    sm: 6,
+    md: 12,
+    lg: 6,
+  };
+
+  const detailsInRow = {
     'Pet Policy': <div className={styles.detailFormat}>{petPolicy}</div>,
     Parking: parking,
+    'Utility Details':'All paid separately'
+  }
+
+  const detailsInRowColConfigurations = {
+    xs: 12,
+    sm: 4,
+    md: 12,
+    lg: 4,
   };
 
   // Changes details specified above into appropriate text + columns
-  const RentAndHouseDetails: FunctionComponent = () => (
+  const RentAndHouseDetails: FunctionComponent<RentAndHouseDetailsProps> = ({details, xs, sm, md, lg}) => (
     <Row className={styles.detailTileWrapper}>
       {Object.entries(details).map(([label, value]) => (
-        <Col xs={12} sm={6} md={12} lg={6}>
+        <Col xs={xs} sm={sm} md={md} lg={lg}>
           <Subtitle2>{label}</Subtitle2>
           <Body2>{value}</Body2>
         </Col>
       ))}
     </Row>
-  );
+  )
 
   // TODO
   // use {utilityDetails} inside here
   const InfoThing = () => <div />;
 
-  const UtilityDetails: FunctionComponent = () => (
-    <Col className={styles.utilityTile}>
-      <div>
-        <Subtitle2>
-          Utility Details <InfoThing />
-        </Subtitle2>
-        <Body2>All paid separately</Body2>
-      </div>
-    </Col>
-  );
-
   const amenityColConfigurations = {
     sm: 6,
     md: 4,
     lg: 3,
-    xl: 2,
+    xl: 4,
   };
 
   const AmenitiesSection: FunctionComponent = () => (
     <Row className={styles.amenitiesWrapper}>
       <Subtitle2>Amenities</Subtitle2>
 
-      <Container>
-        <Body2>
-          <Row>
-            <Amenities
-              selected={facility}
-              {...amenityColConfigurations}
-              className={styles.amenities}
-              colClassName={styles.amenity}
-              extraContent={
-                <Link href={website} external>
-                  More on website
-                </Link>
-              }
-              useCol
-            />
-          </Row>
-        </Body2>
+      <Container className={styles_parent.container}>
+        <Row>
+          <Col xs={9}>
+            <Body2>
+              <Row>
+                <Amenities
+                  selected={facility}
+                  {...amenityColConfigurations}
+                  className={styles.amenities}
+                  colClassName={styles.amenity}
+                  extraContent={
+                    <Link href={website} external>
+                      More on website
+                    </Link>
+                  }
+                  useCol
+                />
+              </Row>
+            </Body2>
+          </Col>
+        </Row>
       </Container>
     </Row>
   );
 
   return (
-    <Container>
+    <Container className={styles_parent.container}>
       <Row>
-        <Col xs={12} md={8} className="pr-md-3">
+        <Col xs={12} lg={8} className="pr-md-3">
           <SectionTitle>About the Place</SectionTitle>
 
           <h5>
@@ -122,25 +152,16 @@ const PlaceDetails: FunctionComponent<PlaceDetailsProps> = ({ roomId }) => {
             </Row>
           </h5>
 
-          <RentAndHouseDetails />
+          <RentAndHouseDetails details={detailsInCol} {...detailsInColColConfigurations}/>
         </Col>
 
         {breakpoint.down.md && <AmenitiesSection />}
 
-        <Col xs={12} lg={4} className={cn(styles.ColWrapper, "pl-md-3")}>
-          {breakpoint.up.lg ? (
-            <>
-              <Contact roomId={roomId} />
-              <UtilityDetails />
-            </>
-          ) : (
-            <>
-              <UtilityDetails />
-              <Contact roomId={roomId} />
-            </>
-          )}
+        <Col xs={12} lg={4} className={cn(styles.ColWrapper, "pl-md-3 order-first order-lg-last")}>
+          <Contact roomId={roomId} />
         </Col>
       </Row>
+      <RentAndHouseDetails details={detailsInRow} {...detailsInRowColConfigurations}/>
 
       {breakpoint.up.lg && <AmenitiesSection />}
     </Container>
