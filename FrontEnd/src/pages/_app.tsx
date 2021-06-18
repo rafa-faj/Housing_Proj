3731f7f6789c34cdfd3a0d265385c3585b910638
Bao Hoang <bghoang@ucsd.edu>
2021-06-17 19:38:44 -0700

    add google tag manager

--- FrontEnd/src/pages/_app.tsx
+++ FrontEnd/src/pages/_app.tsx
@@ -1,15 +1,26 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { reduxNextWrapper } from '@redux';
 import Login from '@components/Login';
 import '../assets/scss/global/index.scss';
 import Layout from '@components/Layout';
-import {InitGA, InitTagManager} from '@components/ga'
+import {InitGA, InitTagManager} from '@components/ga';
+import TagManager from 'react-gtm-module';
 
-InitGA()
-InitTagManager()
-const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
+const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
+  const tagManagerArgs = {
+    gtmId: 'GTM-KRRW5N3',
+    dataLayer: {
+      page: 'home'
+    },
+  };
+  useEffect(()=>{
+    InitGA();
+    TagManager.initialize(tagManagerArgs)
+  }, []);
+  
+  return (
   <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -22,6 +33,6 @@ const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
       <Component {...pageProps} /> 
     </Layout>
   </>
-);
+)};
 
 export default reduxNextWrapper.withRedux(App);
