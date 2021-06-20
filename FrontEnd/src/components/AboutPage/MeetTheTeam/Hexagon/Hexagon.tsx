099a46c2c8e14b1a591e8de28ae22ac3ccf0334b
Amit Bar <amitbar00@gmail.com>
2021-06-19 23:04:44 -0700

    make HexagonPopover into its own component

--- FrontEnd/src/components/AboutPage/MeetTheTeam/Hexagon/Hexagon.tsx
+++ FrontEnd/src/components/AboutPage/MeetTheTeam/Hexagon/Hexagon.tsx
@@ -1,7 +1,39 @@
-import React, { FunctionComponent } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, {
+  FunctionComponent,
+  DetailedHTMLProps,
+  HTMLAttributes,
+  ImgHTMLAttributes,
+} from 'react';
 import styles from './Hexagon.module.scss';
+import HexagonPopover, {
+  HexagonDetails,
+} from './HexagonPopover/HexagonPopover';
 
-const Hexagon: FunctionComponent = () => <div />;
+interface HexagonProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  details?: HexagonDetails;
+  imgSrc?: Pick<
+    DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>,
+    'src'
+  >;
+}
+
+const HexagonUI: FunctionComponent = () => <div className={styles.hexagon} />;
+
+const Hexagon: FunctionComponent<HexagonProps> = ({
+  details,
+  imgSrc,
+  ...divProps
+}) => {
+  if (!details) {
+    return <HexagonUI />;
+  }
+
+  return (
+    <HexagonPopover details={details}>
+      <HexagonUI />
+    </HexagonPopover>
+  );
+};
 
 export default Hexagon;
