64fec8ef6ec3cfc69d427939f6a875ad794f35bf
yizong98 <yijianzong@gmail.com>
2021-07-01 07:44:05 -0700

    finished the third row of about page and cleaned the code

--- FrontEnd/src/components/AboutPage/HomehubPlan/HomehubPlan.tsx
+++ FrontEnd/src/components/AboutPage/HomehubPlan/HomehubPlan.tsx
@@ -1,7 +1,133 @@
 import React, { FunctionComponent } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import styles from './HomehubPlan.module.scss';
+import { aboutUsIcons } from '@icons';
+import useBreakpoints from 'use-window-width-breakpoints';
 
-const HomehubPlan: FunctionComponent = () => <Container></Container>;
+const summer = 'Summer 2021';
+const fall = 'Fall 2021';
+const filter_title = 'Faster with Filter';
+const filter_content =
+  'Input your preferences to get matched more quickly and more accurately with room listings that suit you criteria';
+const bulletin_title = 'Bulletin Board';
+const bulletin_content =
+  'Students will be able to post their own listings to find roommates or pass off a lease';
+const roommate_title = 'Roommate Radar';
+const roommate_content =
+  'Allow students to look for other students to live with. Students will be able to post themselves as potential roommates';
+
+interface HomehubPlanRowDeatils {
+  time: string;
+  title: string;
+  content: string;
+  className: string;
+}
+
+const HomehubPlanRow1Deatils = {
+  time: summer,
+  title: filter_title,
+  content: filter_content,
+  className: styles.plan_1,
+};
+
+const HomehubPlanRow2Deatils = {
+  time: summer,
+  title: bulletin_title,
+  content: bulletin_content,
+  className: styles.plan_2,
+};
+
+const HomehubPlanRow3Deatils = {
+  time: fall,
+  title: roommate_title,
+  content: roommate_content,
+  className: styles.plan_3,
+};
+
+const TimeLineHorizontal: FunctionComponent = () => (
+  <div className={styles.timeline_wrapper}>
+    <div className={styles.timeline}></div>
+    <div className={styles.timedot}>
+      <aboutUsIcons.timeDot />
+    </div>
+  </div>
+);
+
+const TimeLineVertical: FunctionComponent = () => (
+  <Col xs={1} className={styles.timeline_small}>
+    <div className={styles.timedot_small}>
+      <aboutUsIcons.timeDot />
+    </div>
+  </Col>
+);
+
+const PlanTime: FunctionComponent<{ time: string }> = ({ time }) => (
+  <Row className={styles.plan_md_center_wrapper}>
+    <h5 className={styles.plan_time}>{time}</h5>
+  </Row>
+);
+
+const PlanTitle: FunctionComponent<{ title: string }> = ({ title }) => (
+  <Row className={styles.plan_md_center_wrapper}>
+    <h5 className={styles.plan_title}>{title}</h5>
+  </Row>
+);
+
+const PlanContent: FunctionComponent<{ content: string }> = ({ content }) => (
+  <Row className={styles.plan_row}>{content}</Row>
+);
+
+const HomehubPlanSmallRow: FunctionComponent<HomehubPlanRowDeatils> = ({
+  time,
+  title,
+  content,
+  className,
+}) => (
+  <Row className={className}>
+    <TimeLineVertical />
+    <Col>
+      <PlanTime time={time} />
+      <PlanTitle title={title} />
+      <PlanContent content={content} />
+    </Col>
+  </Row>
+);
+
+const HomehubPlanSmall: FunctionComponent = () => (
+  <Container fluid>
+    <HomehubPlanSmallRow {...HomehubPlanRow1Deatils} />
+    <HomehubPlanSmallRow {...HomehubPlanRow2Deatils} />
+    <HomehubPlanSmallRow {...HomehubPlanRow3Deatils} />
+  </Container>
+);
+
+const HomehubPlanBigCol: FunctionComponent<HomehubPlanRowDeatils> = ({
+  time,
+  title,
+  content,
+  className,
+}) => (
+  <Col xs={12} md={4} className={className}>
+    <PlanTitle title={title} />
+    <PlanContent content={content} />
+    <TimeLineHorizontal />
+    <PlanTime time={time} />
+  </Col>
+);
+
+const HomehubPlanBig: FunctionComponent = () => (
+  <Container fluid>
+    <Row>
+    <HomehubPlanBigCol {...HomehubPlanRow1Deatils}/>
+    <HomehubPlanBigCol {...HomehubPlanRow2Deatils}/>
+    <HomehubPlanBigCol {...HomehubPlanRow3Deatils}/>
+    </Row>
+  </Container>
+);
+
+const HomehubPlan: FunctionComponent = () => {
+  const breakpoint = useBreakpoints();
+  return <>{breakpoint.down.sm ? <HomehubPlanSmall /> : <HomehubPlanBig />}</>;
+};
 
 export default HomehubPlan;
