05ffe43cc93d8d42eb8695cfd5df80060e185990
Amit Bar <amitbar00@gmail.com>
2021-07-01 21:04:13 -0700

    undid changes to filledimage

--- FrontEnd/src/components/basics/FilledImage/FilledImage.tsx
+++ FrontEnd/src/components/basics/FilledImage/FilledImage.tsx
@@ -2,32 +2,6 @@ import React, { FunctionComponent } from 'react';
 import styles from './FilledImage.module.scss';
 import cn from 'classnames';
 
-type SafariContainerProps = Pick<
-  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
-  'className' | 'style'
->;
-
-/**
- * Wrapper to provide FilledImage functionality in older browsers
- * (including Safari version 13).
- *
- * Current theory for the core issue is that safari v13 doesn't
- * allow `background-size: cover;` within non absolute elements.
- * So we wrap the `background-size: cover;` with an absolute
- * element and wrap that one with a relatively positioned element.
- */
-const SafariContainer: FunctionComponent<SafariContainerProps> = ({
-  className,
-  style,
-  children,
-}) => (
-  <div className={cn(styles.relativeWrapper, className)} style={style}>
-    <div className={cn(styles.absoluteWrapper, className)} style={style}>
-      {children}
-    </div>
-  </div>
-);
-
 interface FilledImageProps
   extends React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLDivElement>,
@@ -45,9 +19,6 @@ interface FilledImageProps
  *
  * It also provides a hidden image with the same `src` and provided `alt`
  * for any browser/scraping/accessibility reasons.
- *
- * SafariContainer is necessary for FilledImage's support in Safari version 13
- * (and potentially untested older browsers too).
  */
 const FilledImage: FunctionComponent<FilledImageProps> = ({
   src,
@@ -56,15 +27,13 @@ const FilledImage: FunctionComponent<FilledImageProps> = ({
   style,
   ...divProps
 }) => (
-  <SafariContainer className={className} style={style}>
-    <div
-      {...divProps}
-      className={cn(styles.image, className)}
-      style={{ backgroundImage: `url("${src}")`, ...style }}
-    >
-      <img src={src} className={styles.hiddenImage} alt={alt} />
-    </div>
-  </SafariContainer>
+  <div
+    {...divProps}
+    className={cn(styles.image, className)}
+    style={{ backgroundImage: `url("${src}")`, ...style }}
+  >
+    <img src="NA" className={styles.hiddenImage} alt={alt} />
+  </div>
 );
 
 export default FilledImage;
