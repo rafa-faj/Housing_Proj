fafbf1d101ee2c0fe4a853456210c4b5d69b6c58
Rafael Paolo Fajardo <rafael_paolo_fajardo@outlook.com>
2021-08-17 17:40:39 -0700

    Finished logic for Fallback.tsx, need to test

--- FrontEnd/src/components/basics/Fallback/Fallback.tsx
+++ FrontEnd/src/components/basics/Fallback/Fallback.tsx
@@ -1,3 +1,25 @@
 import React, { FunctionComponent } from 'react';
-import { amenityToIcon } from '../Amenities/Amenities';
+import { amenityToIcon, Amenity } from '../Amenities/Amenities';
 import { miscIcons } from '@icons';
+
+interface FallbackProps {
+  amenity: any;
+}
+
+const isAmenity = (key: any): key is Amenity => {
+  if (key in amenityToIcon) {
+    return true;
+  }
+
+  return false;
+};
+
+const FallbackIcon: FunctionComponent<FallbackProps> = ({ amenity }) => {
+  if (isAmenity(amenity)) {
+    return <>{amenityToIcon[amenity]}</>;
+  } else {
+    return <>{miscIcons.Fallback}</>;
+  }
+};
+
+export default FallbackIcon;
